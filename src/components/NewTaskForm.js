0dfc1181afe76f3e89c9739db98b4fbea656dd1e
Red <red.sarouf2014@gmail.com>
2023-06-04 04:51:33 -0400

    add react hooks state events lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ onTaskFormSubmit,categories }) {
+  const [newItemFields, setNewItemFields] = useState({
+    text:'',
+    category:"Code"
+  })
+  function handleFields(e){
+    const{name,value}=e.target
+    setNewItemFields({...newItemFields,[name]:value})
+    
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={e=>{
+      e.preventDefault()
+      onTaskFormSubmit(newItemFields)
+    }}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={newItemFields.text} onChange={handleFields} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select  value={newItemFields.text} onChange={handleFields} name="category">
+          {categories.map((category, i) => (
+            <option key={i}>{category}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, deletedItem}) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, i) => (
+      <Task 
+      deletedItem = {deletedItem} 
+      key={i} 
+      text={task.text}
+      category={task.category}
+      />
+      ))}
+     
     </div>
   );
 }
